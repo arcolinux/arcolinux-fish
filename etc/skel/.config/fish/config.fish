193dd39f79233449b6f45c8c4dd0d71a3e053794
arcolinuxz <arcolinuxinfo@gmail.com>
2022-02-01 18:14:59 +0100

    colors set

--- etc/skel/.config/fish/config.fish
+++ etc/skel/.config/fish/config.fish
@@ -457,58 +457,32 @@ ufetch-arco | lolcat
 
 # colors to set or unset
 
-set fish_color_autosuggestion "#665c54"
+set fish_color_autosuggestion "#969896"
 set fish_color_cancel -r
 set fish_color_command "#83a598"
-set fish_color_comment "#504945"
-set fish_color_cwd green
+set fish_color_comment "#f0c674"
+set fish_color_cwd "#008000"
 set fish_color_cwd_root red
-set fish_color_end "#8ec07c"
+set fish_color_end "#b294bb"
 set fish_color_error "#fb4934"
 set fish_color_escape "#fe8019"
 set fish_color_history_current --bold
-set fish_color_host normal
+set fish_color_host "#85AD82"
 set fish_color_host_remote yellow
 set fish_color_match --background=brblue
 set fish_color_normal normal
 set fish_color_operator "#fe8019"
-set fish_color_param "#ebdbb2"
+set fish_color_param "#81a2be"
 set fish_color_quote "#b8bb26"
 set fish_color_redirection "#d3869b"
-set fish_color_search_match bryellow --background=brblack
-set fish_color_selection white --bold --background=brblack
+set fish_color_search_match bryellow background=brblack
+set fish_color_selection white --bold background=brblack
 set fish_color_status red
 set fish_color_user brgreen
 set fish_color_valid_path --underline
 set fish_pager_color_completion normal
-set fish_pager_color_description "#8ec07c" yellow
+set fish_pager_color_description "#B3A06D" yellow
+set fish_pager_color_prefix normal --bold underline
 set fish_pager_color_prefix white --bold --underline
 set fish_pager_color_progress brwhite --background=cyan
 
-####
-
-set fish_color_command b294bb
-set fish_color_normal normal
-set fish_color_quote b5bd68
-set fish_color_redirection 8abeb7
-set fish_color_end b294bb
-set fish_color_error cc6666
-set fish_color_param 81a2be
-set fish_color_comment f0c674
-set fish_color_match --background=brblue
-set fish_color_selection white --bold background=brblack
-set fish_color_search_match bryellow background=brblack
-set fish_color_history_current --bold
-set fish_color_operator 00a6b2
-set fish_color_escape 00a6b2
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_valid_path --underline
-set fish_color_autosuggestion 969896
-set fish_color_user brgreen
-set fish_color_host normal
-set fish_color_cancel -r
-set fish_pager_color_completion normal
-set fish_pager_color_description B3A06D yellow
-set fish_pager_color_prefix normal --bold underline
-set fish_pager_color_progress brwhite background= 
